1b3a68c102ded26a0bbf9ef61aa57a450ef00e84
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-03-03 09:25:57 -0400

    Inicio del modulo
    Aqui algunos ejercicios hecho el 1 y 2 en mi ejercicio del dia 26: 3/3/2023

---
+++ exercies.js
@@ -0,0 +1,29 @@
+//todo: Ejercicio del dia 26 parte 2: 3/3/2023
+
+//?1-Create a Basic JavaScript Object
+
+/*
+let dog = {
+    name: "pedro",
+    numLegs: 4
+  };
+//Simplemente se mostrara en este objeto las propiedades que tiene adento
+//Tanto el nombre y la cantidad de patas que tiene el perro
+
+console.log(dog);
+*/
+
+//?2-Use Dot Notation to Access the Properties of an Object
+
+/*
+let dog = {
+    name: "Spot",
+    numLegs: 4
+  };
+  console.log(dog.name, dog.numLegs);
+//Aqui estoy llamando del objeto en consola el objeto y la propieda de la misma en .
+  // Only change code below this line
+  */
+
+//?3-Create a Method on an Object
+

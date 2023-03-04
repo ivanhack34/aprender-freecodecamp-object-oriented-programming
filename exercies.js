dbf0d521cee6551bd87a48467ed0948cfd037a1a
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-03-04 10:16:09 -0400

    Ejercicio del dia 27: 4/3/2023
    Aqui algunos ejercicios de este modulo del 3 al 6

--- exercies.js
+++ exercies.js
@@ -25,5 +25,68 @@ let dog = {
   // Only change code below this line
   */
 
+//todo: Ejercicio del dia 27
+
 //?3-Create a Method on an Object
 
+/*
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
+};
+//Lo que hace que en este objeto es llamar una funcion donde retorna una oracion concatenada y de la cual
+//En una parte ponga el valor de ese objeto y a lo que quieres llamar
+//Finalmente retornara el valor de ese objeto.
+
+
+console.log(dog.sayLegs());
+*/
+
+//?4-Make Code More Reusable with the this Keyword
+
+/*
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+//Aqui a diferencia del ejercicio anterior, se usa el this para llamar el objeto directamente
+//Ya despues el proceso sigue siendo igual
+
+
+console.log(dog.sayLegs());
+*/
+
+//?5-Define a Constructor Function
+
+/*
+function Dog() {
+  this.name = "cochi";
+  this.color = "yellow";
+  this.numLegs = 4;
+}
+
+//Lo que se hizo es un constructor, es decir, una forma de hacer un objeto mas comodo.
+//En este caso se crea una funcion  del objeto y luego dentro de la la misma se pone las propiedades de ese contructor.
+*/
+
+
+//?6-Use a Constructor to Create Objects
+
+/*
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+
+let hound = new Dog()
+console.log(hound);
+//Lo que hace que en la siguiente parte ahora se crea una variable para llamar ese constructor
+//Luego en un console.log lo llamamos ese nuevo valor
+// Only change code below this line
+*/
+
+//?7-
